33365504bad25a77f96e3c377bce27c8d1adabca
heyder04 <haydarmuxtar26@gmail.com>
2023-11-19 09:27:48 +0400

    all

--- Ctrl-shift-win/src/app/constants/Endpoints.ts
+++
@@ -1,6 +0,0 @@
-import {environment} from "../../environments/environment";
-
-export const EndpointsConstant = {
-   suggestions:environment.apiUrl+'/feedBack',
-  receipt:environment.apiUrl+'/receipt'
-  }
--- Ctrl-shift-win/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl:'http://10.33.141.38:8080',
-  TRANSLATION :"AIzaSyAfhuxSKGUFdVmL_20Tnh5GiKaPd3fzcW4",
-  OPENAI_API_KEY :"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",
-  GOOGLE_MAP:"AIzaSyBX7oxxfkTgZWPrGSJfIuHcN1bc19UxFSo"
-};
